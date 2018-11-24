4dbd6384bb3887054ac064a433ed5fe6ab9ec27d
Alp <alparslan226@gmail.com>
2018-11-24 06:33:17 +0300

    Update Main.cs

--- Uni-GoyGoy/Uni-GoyGoy.iOS/Main.cs
+++ Uni-GoyGoy/Uni-GoyGoy.iOS/Main.cs
@@ -14,6 +14,8 @@ namespace Uni_GoyGoy.iOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
+            Ekran.Ios_Width = UIScreen.MainScreen.Bounds.Width;
+            Ekran.Ios_Height = UIScreen.MainScreen.Bounds.Height;
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
